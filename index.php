6d387c99fb12e338c0873b86b3f2ebe5e5cf55c4
Dianciré Diallo <diaancir@gmail.com>
2023-06-09 15:12:06 +0200

    Mise en place du CSS

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet">
+    <script defer src="public/js/index.js"></script>
+    <title>Todo</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">TodoList</div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <footer>
+            <p>© Tous droits réservés | 2023</p>
+        </footer>
+    </div>
+</body>
+</html>
