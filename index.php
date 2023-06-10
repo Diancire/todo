082af5ea29ce12c11ace88f48ea5ea182e9f8f04
Dianciré Diallo <diaancir@gmail.com>
2023-06-10 22:57:26 +0200

    Mise en place du formulaire

--- index.php
+++ index.php
@@ -1,28 +1,47 @@
+<?php 
+    const ERROR_REQUIRED = "Veuillez renseigner une todo";
+    const ERROR_TODO_SHORT = "Veuillez entrer au moins 5 caractères";
+    $error = '';
+
+    // Verifie qu'on est bien en méthode post
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $todo = $_POST['todo'] ?? '';
+
+        if(!$todo) {
+            $error = ERROR_REQUIRED;
+        } else if (mb_strlen($todo) < 5) {
+            $error = ERROR_TODO_SHORT;
+        }
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet">
-    <script defer src="public/js/index.js"></script>
+    <?php require_once 'includes/head.php' ?>
     <title>Todo</title>
 </head>
 <body>
     <div class="container">
-        <header>
-            <div class="logo">TodoList</div>
-        </header>
+        <?php require_once 'includes/header.php' ?>
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
-                <div class="todo-form"></div>
+                <form action="" method="post" class="todo-form">
+                    <input name="todo" type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
+                <?php if($error) : ?>
+                    <p class="text-danger"><?= $error; ?></p>
+                <?php endif ?>
                 <div class="todo-list"></div>
             </div>
         </div>
-        <footer>
-            <p>© Tous droits réservés | 2023</p>
-        </footer>
+        <?php require_once 'includes/footer.php' ?>
     </div>
 </body>
 </html>
